dd7112d488150c4cb74d687e3070ec7c8c4ce7c6
mumtoz <140655398+smumt@users.noreply.github.com>
2023-11-14 22:50:16 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,4 @@
 float problemSolution2(float i, float j, float k) {
-    float result;
-    // write your code here;
-
+    float result = (i> j && i> k) ? i : (j>i && j> k) ? j : k;
     return result;
 }
