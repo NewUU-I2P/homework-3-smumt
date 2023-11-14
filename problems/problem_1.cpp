82ed752225cfab2fa9a747e46de0d76a7bb3e50b
mumtoz <140655398+smumt@users.noreply.github.com>
2023-11-14 22:47:47 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,4 +1,4 @@
-void problemSolution1(float consumed_water) {
+float problemSolution1(float consumed_water) {
     float cost=13;
     if (consumed_water<=30){
         cost=cost+consumed_water*0.4;
