67d23e806640dd5126ca36079eacf5b4c9e0dc43
mumtoz <140655398+smumt@users.noreply.github.com>
2023-11-14 13:41:31 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+using namespace std;
 #include "problems/problem_1.cpp"
 #include "problems/problem_2.cpp"
 #include "problems/problem_3.cpp"
 #include "problems/problem_4.cpp"
 #include "problems/problem_5.cpp"
 
-using namespace std;
+
 
 int main() {
     cout << "Problem 1\n";
-    problemSolution1();
+    problemSolution1(55);
     cout << "Problem 2\n";
     problemSolution2();
     cout << "Problem 3\n";
